638d18596e5e6e5c6a5c8762fa2aa771fd5946e4
ImperatorS79 <fevrier.simon@gmail.com>
2021-04-06 14:43:21 +0200

    Remove windows from travis
    No idea why it does not work while locally it works. No time to check that.

--- run/windows/genCBprojectMSYS2.sh
+++ run/windows/genCBprojectMSYS2.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 #Launch this in MinGW64 shell from MSYS2
 
+pacman -S mingw-w64-x86_64-toolchain mingw-w64-x86_64-cmake mingw-w64-x86_64-eigen3 mingw-w64-x86_64-nlohmann-json mingw-w64-x86_64-cgal mingw-w64-x86_64-sol2
+
 buildType="Release"
 if [ ! -z "$2" ]; then
 	buildType="$2"
@@ -35,5 +37,15 @@ mkdir build
 cd build
 
 cmake -G "CodeBlocks - MinGW Makefiles" -DCGAL_HEADER_ONLY=ON -DUSE_MARCH=0 -DCMAKE_SH=SH-NOTFOUND  ..
+
+cp -r ${GMSHSDK}/lib/gmsh-4.7.dll bin
+cp -r ../run/windows/start.bat bin
+cp -r /c/tools/msys64/mingw64/bin/libgcc_s_seh-1.dll bin
+cp -r /c/tools/msys64/mingw64/bin/libgmp-10.dll bin
+cp -r /c/tools/msys64/mingw64/bin/libgmpxx-4.dll bin
+cp -r /c/tools/msys64/mingw64/bin/libgomp-1.dll bin
+cp -r /c/tools/msys64/mingw64/bin/libstdc++-6.dll bin
+cp -r /c/tools/msys64/mingw64/bin/libwinpthread-1.dll bin
+cp -r /c/tools/msys64/mingw64/bin/lua53.dll bin
 
 cd ../
